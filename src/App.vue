75ea4a992815f11da2e63eea10de5e1ecc1a6c22
chgu <chgu@adobe.com>
2019-10-12 07:53:30 -0700

    Initialize project! Fist commit!

---
+++ src/App.vue
@@ -0,0 +1,32 @@
+<template>
+  <div id="app">
+    <landing/>
+  </div>
+</template>
+
+<script>
+  import { App } from './constant/appConstants';
+import Landing from './components/landing.vue'
+
+export default {
+  name: App.APP_NAME,
+  components: {
+    Landing
+  }
+}
+</script>
+
+<style>
+  body {
+    margin: 0px;
+  }
+
+  #app {
+    font-family: 'Avenir', Helvetica, Arial, sans-serif;
+    -webkit-font-smoothing: antialiased;
+    -moz-osx-font-smoothing: grayscale;
+    text-align: center;
+    color: #2c3e50;
+    height: 100%;
+  }
+</style>
