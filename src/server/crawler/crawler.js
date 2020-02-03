555ee35542fbc907241ac8f24f4e5511a30821b8
chgu <chgu@adobe.com>
2020-02-03 07:43:05 -0800

    Update latest data

--- src/server/crawler/crawler.js
+++ src/server/crawler/crawler.js
@@ -1,17 +1,18 @@
-var bunyan = require('bunyan');
+var bunyan = require("bunyan");
 
 // logger for server index
 var logger = bunyan.createLogger({
-    name: 'gitguddojo',
-    streams: [{
-            level: 'debug',
-            path: './log/gitguddojo.log' // log INFO and above to stdout
-        },
-        {
-            level: 'info',
-            path: './data/gitguddojo-data.log' // log data to a file
-        }
-    ]
+  name: "gitguddojo",
+  streams: [
+    {
+      level: "debug",
+      path: "./log/gitguddojo.log" // log INFO and above to stdout
+    },
+    {
+      level: "info",
+      path: "./data/gitguddojo-data.log" // log data to a file
+    }
+  ]
 });
 
 var axios = require("axios");
@@ -20,73 +21,74 @@ var cheerio = require("cheerio");
 var page = 1;
 const server = "r6.tracker.network";
 
-var loadPlayersByPage = (pageIndex) => {
-    var players = [];
+var loadPlayersByPage = async pageIndex => {
+  var players = [];
 
-    var showResult = () => {
-        // result.sort((item1, item2) => item1.date < item2.date);
+  var getData = async i => {
+    try {
+      console.log(
+        "Get user ",
+        players[i].userName,
+        "from ",
+        players[i].userProfileUrl
+      );
 
-        players.forEach((player) => {
-            if (player.operators.length) {
-                logger.info(`${player.userName}:${player.operators.join(':')}`);
-            }
-        });
+      const resp = await axios.get(players[i].userProfileUrl);
 
-        loadPlayersByPage(++page);
-    };
+      {
+        var operators = resp.data.match(/\/images\/badge-[a-z]*\./g);
+        const operatorResult = [];
 
-    var processCount = 0;
-
-    var getData = (i) => {
-        processCount++;
-        players[i].operators = [];
-
-        try {
-            axios.get(players[i].userProfileUrl).then(resp => {
-
-                var operators = resp.data.match(/\/images\/badge-[a-z]*\./g);
-                if (Array.isArray(operators)) {
-                    operators.forEach(operator => {
-                        players[i].operators.push(operator.substring(operator.indexOf('-') + 1, operator.indexOf('.')).toUpperCase());
-                    })
-                }
-
-                if (processCount === players.length) {
-                    showResult();
-                }
-            });
-        } catch (e) {
-            console.log('Cannot get profile ', players[i].userProfileUrl, e);
+        if (Array.isArray(operators)) {
+          operators.forEach(operator => {
+            operatorResult.push(
+              operator
+                .substring(operator.indexOf("-") + 1, operator.indexOf("."))
+                .toUpperCase()
+            );
+          });
         }
+
+        logger.info(`${players[i].userName}:${operatorResult.join(":")}`);
+      }
+    } catch (e) {
+      console.log("Cannot get profile ", players[i].userProfileUrl, e);
     }
+  };
 
-    var getOperator = () => {
-        for (var i = 0; i < players.length; i++) {
-            getData(i);
-        }
-    };
+  var getOperator = async () => {
+    for (var i = 0; i < players.length; i++) {
+      await getData(i);
+    }
+  };
 
-    return axios.get(`https://${server}/leaderboards/pvp-season/all/Mmr?page=${pageIndex}&region=-1&season=16`).then(resp => {
-        var $ = cheerio.load(resp.data);
-        var lis = $("a.trn-lb-entry__name");
+  const resp = await axios.get(
+    `https://${server}/leaderboards/pvp-season/all/Mmr?page=${pageIndex}&region=-1&season=16`
+  );
 
-        try {
-            for (var i = 0; i < lis.length; i++) {
-                const userProfileUrl = `https://${server}${lis[i].attribs.href}`;
-                const userName = lis[i].children[0].data;
+  {
+    var $ = cheerio.load(resp.data);
+    var lis = $("a.trn-lb-entry__name");
 
-                console.log(userName, "*", userProfileUrl)
-                players.push({
-                    userName,
-                    userProfileUrl
-                });
-            }
+    try {
+      for (var i = 0; i < lis.length; i++) {
+        const userProfileUrl = `https://${server}${lis[i].attribs.href}`;
+        const userName = lis[i].children[0].data;
 
-            getOperator();
-        } catch (e) {
-            console.log('Error when processing page ', pageIndex);
-        }
-    });
-}
+        console.log(userName, "*", userProfileUrl);
+        players.push({
+          userName,
+          userProfileUrl
+        });
+      }
+
+      await getOperator();
+    } catch (e) {
+      console.log("Error when processing page ", pageIndex);
+    }
+  }
+
+  await loadPlayersByPage(++page);
+};
 
 loadPlayersByPage(page);
